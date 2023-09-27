Fix gradient function lookup error when higher order gradient functions are referenced in a jax2tf model but not serialized.
PiperOrigin-RevId: 568794459

tf2jax/_src/ops.py
@@ -1044,10 +1044,38 @@ def _gather(proto):
 class _IdentityN(_HigherOrderFunction):
   """Represents a IdentityN Op."""
 
+  name: str
   gradient_op_type: str  # For debug, custom_gradient is handled by _Subgraph.
+  with_custom_gradient: bool
 
   def __call__(self, *args):
-    return args
+    @jax.custom_gradient
+    def _raise_func(
+        *operands: jnp.ndarray,
+    ) -> Tuple[Tuple[jnp.ndarray, ...], Callable[..., Any]]:
+      def grad_fn(_):
+        raise LookupError(
+            f"Custom gradient `{self.gradient_op_type}` was expected but not"
+            f" found for the node `{self.name}` (op type: IdentityN). This"
+            " function is just a placeholder. The subgraph corresponding to"
+            " this IdentityN node should have been wrapped in"
+            " jax.custom_gradient with the actual gradient function found in"
+            " the TensorFlow gradient registry."
+        )
+      return operands, grad_fn
+
+    def _warn_func(*operands: jnp.ndarray) -> Tuple[jnp.ndarray, ...]:
+      logging.warn(
+          "Ignored custom gradient `%s` on the node `%s` (op type: IdentityN).",
+          self.gradient_op_type,
+          self.name,
+      )
+      return operands
+
+    if self.with_custom_gradient:
+      return _raise_func(*args)
+    else:
+      return _warn_func(*args)
 
 
 @register_operation("IdentityN")
@@ -1059,7 +1087,13 @@ def _identity_n(proto):
   if gradient_op_type:
     logging.info("Found custom gradient %s", gradient_op_type)
 
-  return _IdentityN({}, gradient_op_type=gradient_op_type)
+  return _IdentityN(
+      {},
+      name=proto.name,
+      gradient_op_type=gradient_op_type,
+      # Caching the config at conversion time.
+      with_custom_gradient=config.get_config("convert_custom_gradient"),
+  )
 
 
 class _IfOp(_HigherOrderFunction):

tf2jax/_src/tf2jax.py
@@ -790,6 +790,10 @@ def _extract_subgraphs(graphdef, nodes, library):
       grad_fn_name = str(node.attr["_gradient_op_type"].s, "utf-8")
       grad_fn = library[grad_fn_name]
 
+      # The gradient is not available (not serialized in a saved model?)
+      if grad_fn is None:
+        continue
+
       output_node = op_map[node.name][1]
       assert len(node.input) == len(output_node.inputs)
 
@@ -1384,13 +1388,21 @@ def _convert_all_gradient_functions(
 def _convert_gradient_function(
     proto: tf.compat.v1.NodeDef,
     graph: Any,
-    library: Dict[str, _LibraryFunction],
+    library: Dict[str, Optional[_LibraryFunction]],
 ) -> None:
   """Convert a custom_gradient function."""
   op = graph.as_graph_element(proto.name)
   input_specs = tuple([tf.TensorSpec.from_tensor(v) for v in op.inputs])
   grad_fn_name = str(proto.attr["_gradient_op_type"].s, "utf-8")
   if grad_fn_name in library:
+    return
+
+  # Higher order gradient functions may be referenced in JAX2TF produced models
+  # but not actually serialized.
+  try:
+    tf_ops.gradient_registry.lookup(grad_fn_name)
+  except LookupError:
+    library[grad_fn_name] = None
     return
 
   @tf.function