7d06f204361108185c85165fd5a588686cafc0b7
Tom Natan <tomnatan@google.com>
2025-07-08 02:35:45 -0700

    [tf2jax] use V2 sharding if available.
    This is needed when shardy is enabled in JAX, as can be seen in the added test.
    
    PiperOrigin-RevId: 780478506

--- tf2jax/_src/ops.py
+++ tf2jax/_src/ops.py
@@ -2541,13 +2541,18 @@ def _xla_sharding(proto):
     raise ValueError(f"{unspecified_dims=} is not yet supported.")
 
   sharding_str = proto.attr["sharding"].s
+  sharding_v2 = proto.attr["_XlaShardingV2"]
 
   # Return identity if sharding annotation is empty.
   if not sharding_str:
     return lambda x: x
 
   sharding = xla_client.OpSharding()
-  sharding.ParseFromString(sharding_str)
+  if sharding_v2 and sharding_v2.s:
+    sharding.ParseFromString(sharding_v2.s)
+  else:
+    sharding.ParseFromString(sharding_str)
+
   # TODO(shaobohou): Replace with jax.sharding.NamedSharding as GSPMDSharding is
   # deprecated.
   jax_sharding = jex_sharding.GSPMDSharding(jax.devices(), sharding)
