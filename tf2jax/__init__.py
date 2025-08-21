552931ea6de344c2b7f7a6ee225ac58c0a343cbe
Shaobo Hou <shaobohou@google.com>
2025-08-21 06:50:37 -0700

    Bump version to 0.3.9
    PiperOrigin-RevId: 797744553

--- tf2jax/__init__.py
+++ tf2jax/__init__.py
@@ -27,7 +27,7 @@ from tf2jax._src.tf2jax import convert_functional_from_restored
 
 from tf2jax._src.tf2jax import MissingInputError
 
-__version__ = "0.3.8"
+__version__ = "0.3.9"
 
 #  _________________________________________
 # / Please don't use symbols in `_src` they \
